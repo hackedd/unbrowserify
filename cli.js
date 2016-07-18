29407e01ccdea92f27fb04a72f45f5103b619d00
Sean Lang <slang800@gmail.com>
2016-07-18 10:41:12 -0400

    remove wrapper functions
    Node.js has a wrapper function builtin, so you can't pollute the global
    namespace like you can in the browser.

--- cli.js
+++ cli.js
@@ -1,18 +1,14 @@
 #!/usr/bin/env node
 /*jslint node: true */
-(function () {
-    "use strict";
+var unbrowserify = require("./unbrowserify"),
+    filename,
+    outputDirectory;
 
-    var unbrowserify = require("./unbrowserify"),
-        filename,
-        outputDirectory;
+if (process.argv.length <= 2) {
+    console.error("Usage: " + process.argv.join(" ") + " Source [Outdir]");
+    process.exit();
+}
 
-    if (process.argv.length <= 2) {
-        console.error("Usage: " + process.argv.join(" ") + " Source [Outdir]");
-        process.exit();
-    }
-
-    filename = process.argv[2];
-    outputDirectory = process.argv.length >= 4 ? process.argv[3] : ".";
-    unbrowserify.unbrowserify(filename, outputDirectory);
-}());
+filename = process.argv[2];
+outputDirectory = process.argv.length >= 4 ? process.argv[3] : ".";
+unbrowserify.unbrowserify(filename, outputDirectory);
